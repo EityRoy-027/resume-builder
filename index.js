f876162b94bb45c4ce206682d4adedbb25da0793
eityroy <eityroy027@gmail.com>
2026-01-15 02:28:55 +0600

    resumebuilder

--- index.js
+++ index.js
@@ -13,47 +13,40 @@ let linke=document.querySelector('#linkd');
 let proff=document.querySelector('.proff');
 let proffession=document.querySelector('#proffession');
 let jobs=document.querySelector('.inputjobs');
-let postjob1=document.querySelector('.postjob1');
-let postdatejob1=document.querySelector('.postdatejob1');
+let postjob1=document.querySelector('#postjob1');
+let postdatejob1=document.querySelector('#postdatejob1');
 let company=document.querySelector('.inputcompany');
 let sps1=document.querySelector('.sps1');
-let startdate1=document.querySelector('.inputstartdate1');
-let enddate1=document.querySelector('.inputenddate1');
+
+
 let spjob1=document.querySelector('.spjob1');
 let locationcom=document.querySelector('#inputlocationcom');
-let startdate2=document.querySelector('.inputstartdate2');
-let enddate2=document.querySelector('.inputenddate2');
 
- let postdatejob2=document.querySelector('.postdatejob2');
- let postjob2=document.querySelector('.postjob2');
 
- let postedu=document.querySelector('.postedu');
- let postdateedu=document.querySelector('.postdateedu');
+ let postdatejob2=document.querySelector('#postdatejob2');
+ let postjob2=document.querySelector('#postjob2');
+
+ let postedu=document.querySelector('#postedu');
+ let postdateedu=document.querySelector('#postdateedu');
 let work1description=document.querySelector('.descriptionbox1');
 let bottom2=document.querySelector('.bottom2');
+let jobb=document.querySelector('.inputjobs');
+
+let jobtitle1=document.querySelector('#jobtite1');
+let company1=document.querySelector('#company1');
+let startdate1=document.querySelector('#startdate1');
+let enddate1=document.querySelector('#enddate1');
+let descriptionbox1=document.querySelector('#descriptionbox1');
+
+let jobtitle2=document.querySelector('#jobtitle2');
+let company2=document.querySelector('#company2');
+let startdate2=document.querySelector('#startdate2');
+let enddate2=document.querySelector('#enddate2');
+let descriptionbox2=document.querySelector('#descriptionbox2');
+let start;
+let end;
 
 
-
-
-
-
-// let skills=document.querySelector('#skills');
-// let skilled=document.querySelector('#skilled');
-
-// let work2description=document.querySelector('#descriptionwork2');
-// let scldescription=document.querySelector('#descriptionscl1');
-// let scldescription2=document.querySelector('#descriptionscl2');
-//
-// let person=document.querySelector('#person');
-// 
-// 
-// 
-//
-// 
-//
-// let spjob1=document.querySelector('.spjob1');
-// let spjob2=document.querySelector('.spjob2');
-// let spedu=document.querySelector('.spedu');
 
 inname.addEventListener('input',function(){
     nam.textContent=inname.value;
@@ -111,8 +104,8 @@ jobs.addEventListener('input',function(){
     
 })
 
-company.addEventListener('input',function(){
-    spjob1.textContent=company.value;
+company1.addEventListener('input',function(){
+    spjob1.textContent=company1.value;
    postjob1.style.fontSize='0.9rem';
     postjob1.style.color='rgb(53, 52, 52)';
     
@@ -126,31 +119,59 @@ locationcom.addEventListener('input',function(){
 })
 startdate1.addEventListener('input',function(){
     postdatejob1.textContent=startdate1.value;
+    start=startdate1.value;
     postdatejob1.style.fontSize='0.75rem';
      postdatejob1.style.color='rgb(143, 141, 141)';
     
 })
 enddate1.addEventListener('input',function(){
     postdatejob1.textContent=enddate1.value;
+    end=enddate1.value;
+    postdatejob1.style.fontSize='0.75rem';
+     postdatejob1.style.color='rgb(143, 141, 141)';
+    
+})
+enddate1.addEventListener('blur',function(){
+    postdatejob1.textContent=start+ ' - '+ end;
     postdatejob1.style.fontSize='0.75rem';
      postdatejob1.style.color='rgb(143, 141, 141)';
     
 })
 startdate2.addEventListener('input',function(){
-    postdatejob1.textContent=startdate2.value;
-    postdatejob1.style.fontSize='0.75rem';
-     postdatejob1.style.color='rgb(143, 141, 141)';
+    postdatejob2.textContent=startdate2.value;
+    start=startdate2.value;
+    postdatejob2.style.fontSize='0.75rem';
+     postdatejob2.style.color='rgb(143, 141, 141)';
     
 })
 enddate2.addEventListener('input',function(){
-    postdatejob1.textContent=enddate.value2;
-    postdatejob1.style.fontSize='0.75rem';
-     postdatejob1.style.color='rgb(143, 141, 141)';
+    postdatejob2.textContent=enddate2.value;
+    end=enddate2.value;
+    postdatejob2.style.fontSize='0.75rem';
+     postdatejob2.style.color='rgb(143, 141, 141)';
+    
+})
+enddate2.addEventListener('blur',function(){
+    postdatejob2.textContent=startdate2.value+' - '+enddate2.value;
+    postdatejob2.style.fontSize='0.75rem';
+     postdatejob2.style.color='rgb(143, 141, 141)';
     
 })
 work1description.addEventListener('input',function(){
-    bottom2.textContent=work1description.value;
-    bottom2.style.fontSize='0.8rem';
-    bottom2.style.color='rgb(50, 49, 49)';
+    bottom1.textContent=work1description.value;
+    bottom1.style.fontSize='0.8rem';
+    bottom1.style.color='rgb(50, 49, 49)';
+    
+})
+jobtitle2.addEventListener('input',function(){
+    postjob2.textContent=jobtitle2.value;
+   postjob2.style.fontSize='0.9rem';
+    postjob2.style.color='rgb(53, 52, 52)';
+    
+})
+company2.addEventListener('input',function(){
+    spjob2.textContent=company2.value;
+   postjob2.style.fontSize='0.9rem';
+    postjob2.style.color='rgb(53, 52, 52)';
     
 })
